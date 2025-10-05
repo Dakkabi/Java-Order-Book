3088d24ad497792adcfb2ae3fce72e4de598ad4a
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-10-05 21:05:17 +0100

    #3 Create OrderBook and test file

---
+++ order-book/src/main/java/dakkabi/github/models/OrderBook.java
@@ -0,0 +1,8 @@
+package dakkabi.github.models;
+
+/**
+ * Class representation of an order book, holding Order instances with a heap structure.
+ */
+public class OrderBook {
+
+}
---
+++ order-book/src/test/java/dakkabi/github/models/OrderBookTest.java
@@ -0,0 +1,4 @@
+package dakkabi.github.models;
+
+public class OrderBookTest {
+}
