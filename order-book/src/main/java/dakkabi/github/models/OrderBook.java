858598f28fc0f26e9cce5bb16bc6de54c2386432
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-10-05 21:45:51 +0100

    #3 Create basic implementation of OrderBook with minHeap and maxHeap queues and getters.

--- order-book/src/main/java/dakkabi/github/models/OrderBook.java
+++ order-book/src/main/java/dakkabi/github/models/OrderBook.java
@@ -1,8 +1,33 @@
 package dakkabi.github.models;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  * Class representation of an order book, holding Order instances with a heap structure.
  */
 public class OrderBook {
+  private PriorityQueue<Order> bidOrders;
+  private PriorityQueue<Order> askOrders;
 
+  OrderBook() {
+    Comparator<Order> priceDecreasing = new Comparator<Order>() {
+      @Override
+      public int compare(Order order1, Order order2) {
+        return Long.compare(order1.getPrice(), order2.getPrice());
+      }
+    };
+
+    bidOrders = new PriorityQueue<>(priceDecreasing);
+    askOrders = new PriorityQueue<>(priceDecreasing.reversed());
+  }
+
+  public Queue<Order> getBidOrders() {
+    return bidOrders;
+  }
+
+  public Queue<Order> getAskOrders() {
+    return askOrders;
+  }
 }
