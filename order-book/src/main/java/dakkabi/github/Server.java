f40c32cb518d77ce55120d615fb63a8321aa4bc5
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-10-05 19:30:27 +0100

    #4 Add docstring to main method and format Server class

--- order-book/src/main/java/dakkabi/github/Server.java
+++ order-book/src/main/java/dakkabi/github/Server.java
@@ -4,7 +4,12 @@ package dakkabi.github;
  * Entry-point for the Server application.
  */
 public class Server {
-    public static void main(String[] args) {
+  /**
+   * Main runtime entry-point.
+   *
+   * @param args Optional arguments supplied to the program.
+   */
+  public static void main(String[] args) {
 
-    }
+  }
 }
