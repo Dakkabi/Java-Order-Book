323c6175161c60a61d896d62dc9b63342a3ebbf9
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-10-07 20:10:23 +0100

    #7 Rename Server file to GrpcServer to avoid collision with grpc.io.Server object

--- order-book/src/main/java/dakkabi/github/GrpcServer.java
+++ order-book/src/main/java/dakkabi/github/GrpcServer.java
@@ -1,15 +1,19 @@
 package dakkabi.github;
 
+import dakkabi.github.service.OrderBookService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
 /**
  * Entry-point for the Server application.
  */
-public class Server {
+public class GrpcServer {
   /**
    * Main runtime entry-point.
    *
    * @param args Optional arguments supplied to the program.
    */
   public static void main(String[] args) {
-
+    Server server = ServerBuilder.forPort(0).addService(new OrderBookService()).build();
   }
 }
