37c816dde96657aecb70e728818ddbcbc62f9311
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-10-07 20:08:47 +0100

    #7 Create temporary api endpoint service method for createOrder

---
+++ order-book/src/main/java/dakkabi/github/service/OrderBookService.java
@@ -0,0 +1,18 @@
+package dakkabi.github.service;
+
+import dakkabi.github.models.Side;
+import dakkabi.github.proto.CreateOrderResponse;
+import dakkabi.github.proto.OrderBookServiceGrpc.OrderBookServiceImplBase;
+import dakkabi.github.proto.CreateOrderRequest;
+import io.grpc.stub.StreamObserver;
+
+public class OrderBookService extends OrderBookServiceImplBase {
+  @Override
+  public void createOrder(CreateOrderRequest request, StreamObserver<CreateOrderResponse> responseObserver) {
+    Side orderSide = SideMapper.getDomainSide(request.getSide());
+
+    CreateOrderResponse response = CreateOrderResponse.newBuilder().setId(1).build();
+    responseObserver.onNext(response);
+    responseObserver.onCompleted();
+  }
+}
