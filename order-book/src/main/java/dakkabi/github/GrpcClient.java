157f0d26f89886e939bc0383bcd519921a820f49
Dominic <225969620+Dakkabi@users.noreply.github.com>
2025-10-09 21:10:39 +0100

    #10 Create terminal on client startup

--- order-book/src/main/java/dakkabi/github/GrpcClient.java
+++ order-book/src/main/java/dakkabi/github/GrpcClient.java
@@ -1,14 +1,9 @@
 package dakkabi.github;
 
 
-import dakkabi.github.proto.CreateOrderRequest;
-import dakkabi.github.proto.CreateOrderResponse;
-import dakkabi.github.proto.OrderBookServiceGrpc;
-import dakkabi.github.proto.Side;
-import dakkabi.github.proto.Type;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import java.util.Scanner;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import java.io.IOException;
 
 /**
  * A client class to communicate with the gRPC server class.
@@ -19,46 +14,8 @@ public class GrpcClient {
    *
    * @param args Optional additional parameters, does nothing.
    */
-  public static void main(String[] args) {
-    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
-        .usePlaintext()
-        .build();
+  public static void main(String[] args) throws IOException, InterruptedException {
+    Terminal terminal = new DefaultTerminalFactory().createTerminal();
 
-    OrderBookServiceGrpc.OrderBookServiceBlockingStub stub
-        = OrderBookServiceGrpc.newBlockingStub(channel);
-
-    Scanner scanner = new Scanner(System.in);
-
-    while (true) {
-      System.out.println("New Order? (Y/N)");
-      if (scanner.nextLine().equals("N")) {
-        break;
-      }
-
-      System.out.println("Enter Order side:");
-      Side orderSide = Side.valueOf(scanner.nextLine().toUpperCase());
-
-      System.out.println("Enter Order type:");
-      Type orderType = Type.valueOf(scanner.nextLine().toUpperCase());
-
-      System.out.println("Enter Order price:");
-      double price = Long.parseLong(scanner.nextLine());
-
-      System.out.println("Enter Order quantity:");
-      long quantity = Long.parseLong(scanner.nextLine());
-
-      CreateOrderRequest createOrderRequest = CreateOrderRequest.newBuilder()
-          .setSide(orderSide)
-          .setType(orderType)
-          .setPrice(price)
-          .setQuantity(quantity)
-          .build();
-
-      CreateOrderResponse createOrderResponse = stub.createOrder(createOrderRequest);
-
-      System.out.println("Order created, Order ID:" + createOrderResponse.getId());
-    }
-
-    channel.shutdown();
   }
 }
